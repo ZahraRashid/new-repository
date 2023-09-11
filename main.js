ac1a6c7320a1b743fb113d5b311b6c49eea3f60d
ZahraRashid <143653994+ZahraRashid@users.noreply.github.com>
2023-09-11 16:09:40 +0330

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".main-header");
-toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("nav__expanded");
-});
